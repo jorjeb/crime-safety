3672aa7aa8f0e570df9a798794b896f2f903d86f
Jorje Barrera <w.jbarreraact@gmail.com>
2016-01-09 14:58:05 +0800

    Socrate Open Data API integration

--- js/application.js
+++ js/application.js
@@ -1,12 +1,17 @@
 'use strict';
 
-L.mapbox.accessToken = 'pk.eyJ1Ijoiam9yamViYXJyZXJhIiwiYSI6IklWa1JXM1EifQ.BJDcoEfnAsKO7CMvT7CaIw';
+var ACCESS_TOKEN = 'pk.eyJ1Ijoiam9yamViYXJyZXJhIiwiYSI6IklWa1JXM1EifQ.BJDcoEfnAsKO7CMvT7CaIw';
+var MAP_ID = 'jorjebarrera.jlk69o32';
+var DATA_ENDPOINT = 'https://data.sfgov.org/resource/cuks-n6tp.json';
+var APP_TOKEN = 'bRhhd0RZidBn2aCnqgNM1QBJL';
+
+L.mapbox.accessToken = ACCESS_TOKEN;
 
 var southWest = L.latLng(37.7072, -122.5868);
 var northEast = L.latLng(37.8339, -122.3554);
 var bounds = L.latLngBounds(southWest, northEast);
 
-var map = L.mapbox.map('map', 'jorjebarrera.jlk69o32', {
+var map = L.mapbox.map('map', MAP_ID, {
   maxBounds: bounds,
   maxZoom: 20,
   minZoom: 13,
@@ -16,3 +21,43 @@ var map = L.mapbox.map('map', 'jorjebarrera.jlk69o32', {
 map
   .fitBounds(bounds)
   .setView([37.778, -122.426], 13);
+
+$('select').selectpicker({
+  style: 'btn-default'
+});
+
+var date = new Date();
+$('#time').val(date.getHours() + ':' + date.getMinutes());
+
+$('.clockpicker').clockpicker({
+  autoclose: true,
+  placement: 'bottom',
+  twelvehour: true
+});
+
+function loadHeatMap() {
+  var queryString = $('form :input')
+    .filter(function(index, element) {
+      return $(element).val() != '';
+    })
+    .serialize();
+  var url = DATA_ENDPOINT;
+
+  if (queryString != '') {
+    url += '?' + queryString;
+  }
+
+  $.ajax({
+      url: url,
+      beforeSend: function(request) {
+        request.setRequestHeader('X-App-Token', APP_TOKEN);
+      }
+    })
+    .done(function(response) {
+      console.log(response);
+    });
+}
+
+loadHeatMap();
+
+$('#view').on('click', loadHeatMap);
